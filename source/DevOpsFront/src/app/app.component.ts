20f53b9df67613ea0e4be6e5583d2990789f0832
hediane <49047327+hediane@users.noreply.github.com>
2022-05-17 12:12:53 +0200

    code source

---
+++ source/DevOpsFront/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders(
+    {
+      'Content-Type': 'application/json',
+    }
+  )
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent implements OnInit{
+  title = 'Hello DevOps team,';
+  weatherList: any[] = [];
+
+  constructor(  private http: HttpClient) 
+  { 
+
+     
+  }
+
+  ngOnInit(): void {
+
+    this.http.get<any[]>('https://localhost:44313/WeatherForecast' , httpOptions).subscribe(
+      res => {
+        this.weatherList = res;
+        console.log('helooo',res);
+      }
+    )
+  }
+
+}
