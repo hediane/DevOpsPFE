346051bd098e134d39b4d84e3cf748b198b7c088
hediane <49047327+hediane@users.noreply.github.com>
2022-05-30 23:57:34 +0200

    shared library

--- source/DevOpsProject/DevOpsProject/Controllers/WeatherForecastController.cs
+++ source/DevOpsProject/DevOpsProject/Controllers/WeatherForecastController.cs
@@ -22,18 +22,25 @@ namespace DevOpsProject.Controllers
         {
             _logger = logger;
         }
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Hello guys i'm hediane at {date}", DateTime.UtcNow);
+
+            return View();
+        }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
+            {  _logger.LogInformation("Get weather from hediane ", DateTime.UtcNow);
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
         }
+        
     }
 }
