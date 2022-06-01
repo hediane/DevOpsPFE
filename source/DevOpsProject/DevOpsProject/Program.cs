92ca64f52ca7090b652ed961cb476b4c67ef6746
hediane <49047327+hediane@users.noreply.github.com>
2022-06-01 15:03:37 +0200

    shared library

--- source/DevOpsProject/DevOpsProject/Program.cs
+++ source/DevOpsProject/DevOpsProject/Program.cs
@@ -19,62 +19,61 @@ namespace DevOpsProject
             ConfigureLogging();
             CreateHost(args);
         }
-        private static void CreateHost(string[] args)
-        {
-        try
-            {
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (System.Exception ex)
-            {
-                Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
-                throw;
-            }      
-        }
+		private static void ConfigureLogging()
+			{
+				var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+				var configuration = new ConfigurationBuilder()
+					.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+					.AddJsonFile(
+						$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+						optional: true)
+					.Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-	    Host.CreateDefaultBuilder(args)
-		.ConfigureWebHostDefaults(webBuilder =>
-		{
-			webBuilder.UseStartup<Startup>();
-		})
-		.ConfigureAppConfiguration(configuration =>
-		{
-			configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-			configuration.AddJsonFile(
-				$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
-				optional: true);
-		})
-		.UseSerilog();
+				Log.Logger = new LoggerConfiguration()
+					.Enrich.FromLogContext()
+					.Enrich.WithMachineName()
+					.WriteTo.Debug()
+					.WriteTo.Console()
+					.WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+					.Enrich.WithProperty("Environment", environment)
+					.ReadFrom.Configuration(configuration)
+					.CreateLogger();
+			}
 
-        private static void ConfigureLogging()
-        {
-        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-    	var configuration = new ConfigurationBuilder()
-		.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-		.AddJsonFile(
-			$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
-			optional: true)
-		.Build();
-
-	    Log.Logger = new LoggerConfiguration()
-		.WriteTo.File("log-.txt",rollingInterval: RollingInterval.Day)
-		.Enrich.FromLogContext()
-		.Enrich.WithMachineName()
-		.WriteTo.Debug()
-		.WriteTo.Console()
-		.WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
-		.Enrich.WithProperty("Environment", environment)
-		.ReadFrom.Configuration(configuration)
-		.CreateLogger();        }
-
-        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
-        {
-          return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+		private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+			{
+				return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+				{
+					AutoRegisterTemplate = true,
+					IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+				};
+			}
+			private static void CreateHost(string[] args)
+{
+	try
 	{
-		AutoRegisterTemplate = true,
-		IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
-	};
-        }
-    }
+		CreateHostBuilder(args).Build().Run();
+	}
+	catch (System.Exception ex)
+	{
+		Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+		throw;
+	}
+}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			   Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				})
+				.ConfigureAppConfiguration(configuration =>
+				{
+					configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+					configuration.AddJsonFile(
+						$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+						optional: true);
+				})
+				.UseSerilog();
+			}
 }
