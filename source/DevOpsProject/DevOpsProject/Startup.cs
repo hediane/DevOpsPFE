1139d4367b3dba49a36bdbcc4218b2d72bae5cb5
hediane <49047327+hediane@users.noreply.github.com>
2022-06-13 16:13:50 +0200

    test

--- source/DevOpsProject/DevOpsProject/Startup.cs
+++ source/DevOpsProject/DevOpsProject/Startup.cs
@@ -42,7 +42,7 @@ namespace DevOpsProject
                 });
             });
         }
-
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
