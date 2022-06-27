e0c6ddf21c9eb5ec0efe0f3fcce58253f7f50bbd
hediane <49047327+hediane@users.noreply.github.com>
2022-06-27 13:46:23 +0200

    up

--- source/DevOpsProject/Controllers/WeatherForecastController.cs
+++ source/DevOpsProject/Controllers/WeatherForecastController.cs
@@ -2,11 +2,9 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Linq;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
+
 namespace DevOpsProject.Controllers
 {
     [ApiController]
