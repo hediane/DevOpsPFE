eb2e3273284ba2530a7f17d37b7baa470ef816e9
hediane <49047327+hediane@users.noreply.github.com>
2022-07-06 13:15:43 +0200

    up

---
+++ source/TestProject/ClassTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class ClassTest
+    {
+        private double balance;
+
+        public ClassTest()
+        {
+        }
+
+        public ClassTest(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        public void Add(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > balance)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            balance -= amount;
+        }
+
+        public void TransferFundsTo(ClassTest otherAccount, double amount)
+        {
+            if (otherAccount is null)
+            {
+                throw new ArgumentNullException(nameof(otherAccount));
+            }
+
+            Withdraw(amount);
+            otherAccount.Add(amount);
+        }
+    }
+}
---
+++ source/nunitTest/NunitClass.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace nunitTest
+{
+    public class Tests
+    {
+        private TestProject.ClassTest account;
+
+        [SetUp]
+        public void Setup()
+        {
+            account = new TestProject.ClassTest(2000);
+        }
+
+        [Test]
+        public void Adding_funds_updates_Balance()
+        {
+            //var account = new bankTest.BankAccount(2000);
+            account.Add(200);
+            Assert.AreEqual(2200, account.Balance);
+
+        }
+
+        [Test]
+        public void Withdrawing_funds_updates_Balance()
+        {
+            account.Withdraw(200);
+            Assert.AreEqual(1800, account.Balance);
+
+        }
+
+        [Test]
+        public void Transfering_funds_updates_Balance()
+        {
+
+            var accountTransfered = new TestProject.ClassTest(1000);
+            account.TransferFundsTo(accountTransfered, 200);
+            Assert.AreEqual(1200, accountTransfered.Balance);
+
+        }
+    }
+}
