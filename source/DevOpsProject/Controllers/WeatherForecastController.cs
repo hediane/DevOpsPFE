09ca99eb7406d3533b0910622d952ddae21fc15f
hediane <49047327+hediane@users.noreply.github.com>
2022-07-06 22:39:22 +0200

    up

--- source/DevOpsProject/Controllers/WeatherForecastController.cs
+++ source/DevOpsProject/Controllers/WeatherForecastController.cs
@@ -21,14 +21,17 @@ namespace DevOpsProject.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            _logger.LogInformation("Hello guys , we are Devops teams");
+
         }
+
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
-        {   _logger.LogInformation("Hello guys , we are Devops teams");
+        {   
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
+            {  
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
