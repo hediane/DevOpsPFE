f500c3c96687c66e3538d1bcb9f88cc4ba643a88
hediane <49047327+hediane@users.noreply.github.com>
2022-07-06 15:39:48 +0200

    up

--- source/DevOpsProject/Program.cs
+++ source/DevOpsProject/Program.cs
@@ -12,49 +12,8 @@ namespace DevOpsProject
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        var configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{environment}.json", optional: true)
-            .Build();
-        
-        ConfigureLogging(environment, configuration);
-
-        try
-        {
-            Log.Information("Starting host.");
             CreateHostBuilder(args).Build().Run();
         }
-        catch (Exception ex)
-        {
-            Log.Fatal(ex, "Host failure.");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
-
-        
-        }
-     private static void ConfigureLogging(string environment, IConfigurationRoot configuration)
-    {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
-            .Enrich.FromLogContext()
-            .Enrich.WithEnvironmentName()
-            .Enrich.WithMachineName()
-            .WriteTo.Console()
-            .WriteTo.Debug()
-            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticSearch:Url"]))
-            {
-                AutoRegisterTemplate = true,
-                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
-                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name!.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
-            })
-            .ReadFrom.Configuration(configuration)
-            .CreateLogger();
-    }
 
     private static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
